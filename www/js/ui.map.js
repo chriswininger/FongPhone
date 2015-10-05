9236c151ed5922e2f3512b026a1ac1abb29b05ad
trouscaillon <andrew.r.allred@gmail.com>
2015-10-04 22:35:08 -0400

    UI tweak

--- www/js/ui.map.js
+++ www/js/ui.map.js
@@ -34,6 +34,8 @@ var _baseNote = 'a4';
 		var rowSize = 1;
 
 		window.PhonePhong.UI.NoteMap = function ($scope, $window) {
+			
+			$('#mapUI').css('max-height', (window.innerHeight - 60) + "px"); 
 
 			//$scope.NoteMap = _availableNotes;
 			$scope.windowHeight = $window.innerHeight;
