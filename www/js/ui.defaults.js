d72a60e0f1a7dd930e971d79dd73cbe8efd0abb1
chriswininger <chris.wininger@gmail.com>
2015-10-11 12:39:35 -0400

    Implemented ui pad state storage
    Fong postions and attributes for the ui pad will be stored through localforage
     and restored on app launch or view re-initializaion
    Moved initial settings into default file

---
+++ www/js/ui.defaults.js
@@ -0,0 +1,56 @@
+(function() {
+  window.FongPhone = window.FongPhone || {};
+  window.FongPhone.UI = window.FongPhone.UI || {};
+  window.FongPhone.UI.Defaults = {
+    fongDots: [{
+      x: 60,
+      y: 60,
+      radius: 60,
+      color: "#ded6d6",
+      fadeOffset: 0,
+      selectedClassType: true,
+      selectedState: "sine",
+      gradient: 'grad1',
+      domCtxID: "phongUIGrid",
+      elementID: "oscTouch1",
+      fadeElementID: "oscTouch1Fade",
+      boardInputIndex: 0,
+      fongRole: 'primary',
+      states: [
+        "sine",
+        "square",
+        "triangle",
+        "sawtooth"
+      ],
+      classes: [
+        true,
+        false
+      ]
+    },
+    {
+      x: 200,
+      y: 200,
+      radius: 60,
+      color: "#ded6d6",
+      fadeOffset: 0,
+      selectedClassType: true,
+      selectedState: "sine",
+      gradient: 'grad2',
+      domCtxID: "phongUIGrid",
+      elementID: "oscTouch2",
+      fadeElementID: "oscTouch2Fade",
+      boardInputIndex: 1,
+      fongRole: 'secondary',
+      states: [
+        "sine",
+        "square",
+        "triangle",
+        "sawtooth"
+      ],
+      classes: [
+        true,
+        false
+      ]
+    }]
+  }
+})();
