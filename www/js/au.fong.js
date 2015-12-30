a77423955f5670706bed7290747255436ba97b18
trouscaillon <andrew.r.allred@gmail.com>
2015-12-30 00:36:10 -0500

    Resetting fong code to last stable version
    Subsequent changes from b1c909d32ed8096120c2928cc36080919705e3f1 on
    crash the app in Safari and iOS

--- www/js/au.fong.js
+++ www/js/au.fong.js
@@ -1,5 +1,3 @@
-var TEMP_BAD = [];
-
 function fong(audCtx, mainVol, x, y, board) {
 	var self = this;
 	this.board = board;
@@ -33,52 +31,66 @@ function fong(audCtx, mainVol, x, y, board) {
 	this.radius = 60;
 
 	this.osc = audCtx.createOscillator();
-	this.oscPanCtrl = audCtx.createPanner();
-	this.oscVol = audCtx.createGain();
-	this.oscVolOffset = audCtx.createGain();
-	this.oscGainCtrl = audCtx.createOscillator();
-	this.filter = audCtx.createBiquadFilter();
-	this.delay = audCtx.createDelay(10);
-	this.feedback = audCtx.createGain();
-	this.delayGain = audCtx.createGain();
-	TEMP_BAD.push(this.oscPanCtrl);
 	this.osc.type = 'sine';
 
 	this.oscs = [];
 	this.oscsCount = 0;
 	this.oscsIncrement = 2;
 
+	for (var i = 0; i < this.oscsCount; i++) {
+		this.oscs[i] = audCtx.createOscillator();
+		this.oscs[i].type = 'sine';
+	}
+
+	this.oscPanCtrl = audCtx.createPanner();
+	this.oscVol = audCtx.createGain();
+	this.oscVolOffset = audCtx.createGain();
+
 	this.oscVolOffset.gain.value = 1.0;
+
+	this.oscGainCtrl = audCtx.createOscillator();
+
 	this.oscGainCtrl.type = 'square';
+
+	this.oscGainCtrl.connect(this.oscVol.gain);
+
+	this.filter = audCtx.createBiquadFilter();
 
 	this.filter.type = 'lowpass'; // In this case it's a lowshelf filter
 	this.filter.frequency.value = 200;
 	this.filter.Q.value = 0;
 	this.filter.gain.value = 1;
 
+	this.osc.connect(this.filter);
+
+	for (var i = 0; i < this.oscsCount; i++) {
+		this.oscs[i].connect(this.filter);
+	}
+
+	this.filter.connect(this.oscVol);
+	this.oscVol.connect(this.oscVolOffset);
+
+	this.oscVolOffset.connect(this.oscPanCtrl);
+
+	this.delay = audCtx.createDelay(10);
 	this.delay.delayTime.value = this.board.delayTime;
+
+	this.feedback = audCtx.createGain();
 	this.feedback.gain.value = this.board.delayFeedback;
+
+	this.delayGain = audCtx.createGain();
 	this.delayGain.gain.value = this.board.delayVolume;
 
-	this.oscGainCtrl.connect(this.oscVol.gain);
-	this.osc.connect(this.oscVol);
-	this.oscVol.connect(this.filter);
 	this.delay.connect(this.delayGain);
 	this.delayGain.connect(this.feedback);
 	this.feedback.connect(this.delay);
 
-	//this.oscPanCtrl.connect(mainVol);
-	//this.oscVolOffset.connect(mainVol);
-	this.filter.connect(this.oscVolOffset);
-
-	// connect final output with all filters/fades applied back into delay/feedback loop
-	this.oscVolOffset.connect(this.delay);
-
-	this.delayGain.connect(this.oscVolOffset); // should be delayGain to oscVolOffset
-
-	this.oscVolOffset.connect(this.oscPanCtrl);
+	this.oscPanCtrl.connect(this.delay);
 
 	this.oscPanCtrl.connect(mainVol);
+	this.delay.connect(mainVol);
+
+	mainVol.connect(audCtx.destination);
 
 	this.start = function () {
 		this.osc.start(0);
@@ -87,7 +99,7 @@ function fong(audCtx, mainVol, x, y, board) {
 		for (var i = 0; i < this.oscsCount; i++) {
 			this.oscs[i].start();
 		}
-	};
+	}
 
 	this.setOscType = function (type) {
 		this.osc.type = type;
@@ -97,7 +109,7 @@ function fong(audCtx, mainVol, x, y, board) {
 		this.waveIntOsc++;
 		if (this.waveIntOsc >= this.waves.length) this.waveIntOsc = 0;
 		this.setOscType(this.waves[this.waveIntOsc]);
-	};
+	}
 
 	this.stopOscPulse = function () {
 		this.oscGainCtrl.disconnect(this.oscVol.gain);
@@ -112,11 +124,12 @@ function fong(audCtx, mainVol, x, y, board) {
 	this.toggleOscPulse = function () {
 		if (this.oscPulseOn) this.stopOscPulse();
 		else this.startOscPulse();
-	};
+	}
 
 	this.setOscVol = function (vol) {
 		vol = vol / 3;
-		this.oscVol.gain.value = vol;
+		this.oscVol.gain = vol;
+		this.oscVolOffset.gain.value = vol;
 	};
 
 	this.setOscFreq = function (freq) {
@@ -125,11 +138,22 @@ function fong(audCtx, mainVol, x, y, board) {
 		this.oscFreq = freq;
 		if (this.board.portamento > 0) {
 			var dur = this.board.portamento / 1000.0;
-			if (freq > 0) {
+			if (freq > 0)
+			{
 				this.osc.frequency.exponentialRampToValueAtTime(freq, this.audCtx.currentTime + dur);
+			}
+
+			for (var i = 0; i < this.oscsCount; i++) {
+				if (freq * (i + 1 * this.oscsIncrement) > 0)
+				{
+					this.oscs[i].frequency.exponentialRampToValueAtTime(freq * (i + 1 * this.oscsIncrement), this.audCtx.currentTime + dur);
+				}
 			}
 		} else {
 			this.osc.frequency.value = freq;
+			for (var i = 0; i < this.oscsCount; i++) {
+				this.oscs[i].frequency.value = freq * (i + 1 * this.oscsIncrement);
+			}
 		}
 	};
 
@@ -148,11 +172,18 @@ function fong(audCtx, mainVol, x, y, board) {
 		this.filter.Q.value = q;
 	};
 
+	this.oscOff = function () {
+		this.oscVol.disconnect(this.audCtx.destination);
+	};
+
+	this.oscOn = function () {
+		this.oscVol.connect(this.audCtx.destination);
+	};
+
 	this.turnFilterOn = function () {
 		try {
-			this.oscVol.disconnect(this.oscVolOffset);
-			this.oscVol.connect(this.filter);
-			this.filter.connect(this.oscVolOffset);
+			this.osc.disconnect(this.oscVol);
+			this.osc.connect(this.filter);
 		} catch (ex) {
 			if (ex.code === 15) return; // not off
 			console.error(ex);
@@ -161,9 +192,8 @@ function fong(audCtx, mainVol, x, y, board) {
 
 	this.turnFilterOff = function () {
 		try {
-			this.oscVol.disconnect(this.filter);
-			this.filter.disconnect(this.oscVolOffset);
-			this.oscVol.connect(this.oscVolOffset);
+			this.osc.disconnect(this.filter);
+			this.osc.connect(this.oscVol);
 		} catch (ex) {
 			if (ex.code === 15) return; // not on
 			console.error(ex);
@@ -182,8 +212,8 @@ function fong(audCtx, mainVol, x, y, board) {
 		this.feedback.gain.value = val;
 	};
 
-	this.setFade = function (x, y, z) {
-		this.oscPanCtrl.setPosition(x, y, z);
+	this.setFade = function (val) {
+		this.oscPanCtrl.setPosition(val, 0, 0);
 	};
 
 	this.setFilterType = function (type) {
